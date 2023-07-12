7588190491a4733c0a5665c84bf40d343d77aab3
Festus Daramola <123788749+afdaraola@users.noreply.github.com>
2023-07-12 07:39:21 -0400

    Update count-john.sql

--- count-john.sql
+++ count-john.sql
@@ -3,7 +3,8 @@ Given the following data definition, write a query that returns the number of st
 TABLE students
    id INTEGER PRIMARY KEY,
    firstName VARCHAR(30) NOT NULL,
-   lastName VARCHAR(30) NOT NULL
+   lastName VARCHAR(30) NOT NULL 
+--
  */
    
 select count(*) as noOfStudents
